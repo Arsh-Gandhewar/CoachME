5bd1de812a183b28102caa6d850f1d73cff21c97
Arsh Gandhewar <arshgandhewar5650@gmail.com>
2026-06-03 23:43:13 +0530

    chore: update API URLs to match new Wi-Fi IP address

--- client/constants/config.ts
+++ client/constants/config.ts
@@ -1,5 +1,5 @@
-export const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL || 'http://10.199.211.209:5000/api';
-export const SOCKET_URL = process.env.EXPO_PUBLIC_SOCKET_URL || 'http://10.199.211.209:5000';
+export const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL || 'http://10.210.116.209:5000/api';
+export const SOCKET_URL = process.env.EXPO_PUBLIC_SOCKET_URL || 'http://10.210.116.209:5000';
 export const APP_NAME = 'CoachME';
 export const APP_VERSION = '1.0.0';
 
